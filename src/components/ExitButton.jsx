f0b12967936b4d6455cf582566502643499028f8
Jose Alberto <josealbertoholgado@gmail.com>
2023-06-07 17:07:54 +0200

    Now, the exit button needs confirmation

--- src/components/ExitButton.jsx
+++ src/components/ExitButton.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import { IconButton, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { IconButton, Box, Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
-function ExitButton (props){
-    return(
-        <Box sx={{display: 'flex', flexDirection: 'row-reverse'}}>
-                <IconButton onClick={props.finishExam} size='large'>
-                    <HighlightOffIcon fontSize='inherit'/>
-                </IconButton>
-            </Box>
-    )
+function ExitButton(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = () => {
+    setOpen(false);
+    props.finishExam();
+  };
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+      <IconButton onClick={handleClick} size='large'>
+        <HighlightOffIcon fontSize='inherit' />
+      </IconButton>
+
+      {/* Warning dialog */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Alerta</DialogTitle>
+        <DialogContent>
+          <Typography>¿Estás seguro que quieres salir?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleConfirm} color='error'>Salir</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 }
 
 export default ExitButton;
